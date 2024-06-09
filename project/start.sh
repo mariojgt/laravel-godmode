f55c1cacf29798ea395f1da8232420462123b455
Mario Tarosso <mariojgt2@gmail.com>
2024-06-09 13:17:36 +0000

    updates

--- project/start.sh
+++ project/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -e
 
-# Ensure the storage and cache directories have the correct permissions
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+# Add rules you may need to run before the project starts
 
-# Execute the original entrypoint command
-exec "$@"
